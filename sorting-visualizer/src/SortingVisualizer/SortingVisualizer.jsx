851c4c2a8967f79de73127f01b9c2bda67aeaac4
William Garcia-Sanchez <89278772+Wixx19@users.noreply.github.com>
2024-06-03 18:44:15 -0400

    working on mergesort

---
+++ sorting-visualizer/src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import * as sortingAlgorithms from '../sortingAlgorithms/sortingAlgorithms.js';
+import './SortingVisualizer.css';
+
+
+export default class SortingVisualizer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            array: [],
+        };
+    }
+
+    componentDidMount(){
+        this.resetArray();
+    }
+
+    resetArray() {
+        const array = [];
+        for (let i = 0; i < 400; i++) {
+            array.push(randomIntFromInterval(5,730));
+        }  
+        this.setState({array});
+    }
+
+    mergeSort() {
+        const jsSortedArray = this.state.array
+            .slice()
+            .sort((a,b) => a - b);
+        const sortedArray = sortingAlgorithms.mergeSort(this.state.array);
+
+        console.log(arraysAreEqual(jsSortedArray, sortedArray));
+    }
+
+    quickSort() {}
+
+    heapSort() {}
+
+    bubbleSort() {}
+
+    render(){
+        const {array} = this.state;
+
+        return (
+            <div className="array-container">
+                {array.map((value, idx) => (
+                    <div 
+                        className="array-bar" 
+                        key={idx}
+                        style={{height: `${value}px`}}>
+                    </div>
+                ))}
+                <button onClick={() => this.resetArray()}>Generate New Array</button>
+                <button onClick={() => this.mergeSort()}>Merge Sort</button>
+                <button onClick={() => this.quickSort()}>Quick Sort</button>
+                <button onClick={() => this.heapSort()}>Heap Sort</button>
+                <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+            </div>
+        );
+    }
+
+}
+
+
+function randomIntFromInterval(min,max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function arraysAreEqual(arrayOne, arrayTwo) {
+    if (arrayOne.length !== arrayTwo.length) return false;
+    for (let i = 0; i < arrayOne.length; i++){
+        if (arrayOne[i] !== arrayTwo[i]) return false;
+    }
+    return true;
+}
